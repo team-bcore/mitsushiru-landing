124f8886e1d697e2b51d6edf0ebfd9d7a3bea044
Satomi <s-tanaka@bcore.co.jp>
2021-11-26 10:59:51 +0900

    Implement Support component

--- components/Support.js
+++ components/Support.js
@@ -4,11 +4,39 @@ export default function Features() {
     return (
     
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-            <div className="">
-                <p className="p-8 text-2xl text-center font-bold md:text-3xl ">
-                    🟨 安心のサポート体制
-                </p>
-            </div>         
+          <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+                    <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+                        <span class="relative inline-block">
+                        <svg
+                  viewBox="0 0 52 24"
+                  fill="currentColor"
+                  class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-green-600 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                >
+                  <defs>
+                    <pattern
+                      id="232db96b-4aa2-422f-9086-5a77996d1df1"
+                      x="0"
+                      y="0"
+                      width=".135"
+                      height=".30"
+                    >
+                      <circle cx="1" cy="1" r=".7" />
+                    </pattern>
+                  </defs>
+                  <rect
+                    fill="url(#232db96b-4aa2-422f-9086-5a77996d1df1)"
+                    width="52"
+                    height="24"
+                  />
+                </svg>
+                            <span class="relative">安心のサポート体制</span>
+                        </span>{' '}
+
+                    </h2>
+                    <p class="text-base text-gray-700 md:text-lg">
+                        導入してからも、ビーコアがしっかりサポートしますので、<br></br>安心してサービスをご利用いただけます。
+                    </p>
+                </div>       
         <div className="grid gap-8 row-gap-5 md:grid-cols-2">
           <div className="relative p-px overflow-hidden transition duration-300 transform border rounded shadow-sm">
             <div className="relative flex flex-col h-full p-5 bg-white rounded-sm lg:items-center lg:flex-row">
@@ -39,13 +67,15 @@ export default function Features() {
             
             <div className="relative flex flex-col h-full p-5 bg-white rounded-sm lg:items-center lg:flex-row">
               <div className="mb-6 mr-6 lg:mb-0">
-                <div className="flex items-center justify-center ">
+                <div className="flex items-center justify-center">
+                <a href="https://privacymark.jp/">
                 <Image
                         src="/images/Support/privacy-mark.png" // Route of the image file
                         height={251} // Desired size with correct aspect ratio
                         width={351} // Desired size with correct aspect ratio
                         alt="プライバシーマーク"
                     />
+                  </a>
                 </div>
               </div>
               <div className="flex flex-col justify-between flex-grow">
