2471c853d570a318882b870d0ae6ddc4c89122c7
Satomi <s-tanaka@bcore.co.jp>
2021-11-26 10:58:37 +0900

    Change text propaties

--- components/Recommend.js
+++ components/Recommend.js
@@ -4,14 +4,16 @@ export default function Recommend() {
     return(
 
         <div className="bg-primary pt-16 pb-16">
-            <div className="px-2">
-                <p className="text-2xl text-center font-bold md:text-3xl">
+           <div className="px-2">
+                <p className="text-3xl text-center font-bold text-gray-900 md:text-4xl">
                     🟨 このような場所におすすめします
                 </p>
-                <p className="text-center pt-12 md:mx-24">
+                <p className="text-center pt-12 text-gray-700 md:mx-24">
                 病院、大浴場やスポーツジム、飲食店など、不特定多数が集まる、入口と出口が明確な場所が適しています。
-                </p>
-                <div className="grid grid-cols-1 justify-items-center pt-9 md:grid md:grid-cols-6">
+                </p> 
+                </div>    
+            <div className="px-2">
+                <div className="pt-6 grid grid-cols-1 justify-items-center md:grid md:grid-cols-6">
                  <div className="md:col-start-2 md:col-span-4 lg:px-16">  
                 <Image
                         src="/images/Recommend/recommend-image.svg" // Route of the image file
