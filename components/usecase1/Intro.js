d12fb98a2594c7310acd2b1b68747c4ecf3b4e33
Satomi <s-tanaka@bcore.co.jp>
2021-12-15 14:16:58 +0900

    Put space between text

--- components/usecase1/Intro.js
+++ components/usecase1/Intro.js
@@ -14,13 +14,13 @@ export default function Intro() {
             <div className="flex items-center justify-center w-12 h-12 rounded-full bg-primary"></div>
           </a>
           <h5 className="mb-4 text-4xl font-extrabold leading-none">
-            医療法人社団法山会
+            医療法人社団 法山会
             <br className="hidden md:block" />
             山下診療所　大塚{" "}
             <span className="inline-block text-deep-purple-accent-400"></span>
           </h5>
           <hr className="mb-5 border-gray-300" />
-          <p className="mb-6 text-gray-700">
+          <p className="mb-6 text-gray-700 leading-relaxed">
             山下診療所は、医科（内科・小児科・耳鼻咽喉科）と歯科（歯科・口腔外科）が併設された大塚と自由が丘駅前に立地する都市型のクリニックです。
             多数の専門医が勤務し、多角的な視点で高精度な医療をワンストップで提供しています。オンライン診療にも早くから取り組み、最新技術の導入には意欲
             的です。今回はミツシルを大塚病院に導入いただきました。
