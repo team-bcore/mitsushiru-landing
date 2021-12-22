69a015e9224e6a317f7473cb6c8dfd2f9bea11da
Satomi <s-tanaka@bcore.co.jp>
2021-12-22 10:43:01 +0900

    Change image

--- components/usecase1/Comment1.js
+++ components/usecase1/Comment1.js
@@ -2,24 +2,22 @@ import Image from "next/image";
 
 export default function Comment1() {
   return (
+    
+
+
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="flex flex-col max-w-screen-lg overflow-hidden bg-white border rounded shadow-sm lg:flex-row sm:mx-auto">
+      <div className="flex flex-col max-w-screen-lg overflow-hidden bg-white lg:flex-row sm:mx-auto">
         <div className="relative lg:w-1/2">
           <Image
-            src="/images/Usecase1/output1.jpg" // Route of the image file
-            height={2532} // Desired size with correct aspect ratio
-            width={2026} // Desired size with correct aspect ratio
+            className="rounded"
+            src="/images/Usecase1/Yamashita_Iwao_portrait.jpeg" // Route of the image file
+            height={1280} // Desired size with correct aspect ratio
+            width={1280} // Desired size with correct aspect ratio
             alt="山下 巌 先生"
           />
-          <svg
-            className="absolute top-0 right-0 hidden h-full text-white lg:inline-block"
-            viewBox="0 0 20 104"
-            fill="currentColor"
-          >
-            <polygon points="17.3036738 5.68434189e-14 20 5.68434189e-14 20 104 0.824555778 104" />
-          </svg>
+           
         </div>
-        <div className="flex flex-col justify-center p-8 bg-white lg:p-16 lg:pl-10 lg:w-1/2">
+        <div className="flex flex-col justify-center p-8 bg-white lg:px-8 lg:pl-12 lg:w-1/2">
           <div>
             <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-primary">
               お客様の声
