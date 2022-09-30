8b22737b52b18a7711e9cb1fff63d58667e065de
Satomi <s-tanaka@bcore.co.jp>
2022-09-30 15:57:39 +0900

    add file

---
+++ components/usecase3/Top.js
@@ -0,0 +1,27 @@
+export default function Top() {
+    return (
+        <div className="relative bg-primary">
+            <div className="absolute inset-x-0 bottom-0">
+                <svg
+                    viewBox="0 0 224 12"
+                    fill="currentColor"
+                    className="w-full -mb-1 text-white"
+                    preserveAspectRatio="none"
+                >
+                    <path d="M0,0 C48.8902582,6.27314026 86.2235915,9.40971039 112,9.40971039 C137.776408,9.40971039 175.109742,6.27314026 224,0 L224,12.0441132 L0,12.0441132 L0,0 Z" />
+                </svg>
+            </div>
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                <div className="mt-16 relative max-w-2xl sm:mx-auto sm:max-w-xl md:max-w-2xl sm:text-center">
+                    <h2 className="mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+                        密 ミツシル 導⼊事例 3
+                    </h2>
+                    <p className="mb-6 text-base text-white md:text-lg">
+                    実際にミツシルを導⼊されたお客様からの声をご紹介しています
+                    </p>
+                   
+                </div>
+            </div>
+        </div>
+    );
+}
