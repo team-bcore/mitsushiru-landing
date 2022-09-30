de6b6e46f6cb921d6ab1d83a62af484797925aac
Satomi <s-tanaka@bcore.co.jp>
2022-09-30 15:56:50 +0900

    text

--- components/usecase2/CommentEW.js
+++ components/usecase2/CommentEW.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Comment2() {
+export default function CommentEW() {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="grid gap-10 lg:grid-cols-2">
