b98e1e705b56cf9381391735155820525d0c94b2
Satomi <s-tanaka@bcore.co.jp>
2022-09-30 15:56:33 +0900

    remove space

--- components/usecase1/Comment1.js
+++ components/usecase1/Comment1.js
@@ -2,9 +2,6 @@ import Image from "next/image";
 
 export default function Comment1() {
   return (
-    
-
-
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="flex flex-col max-w-screen-lg overflow-hidden bg-white lg:flex-row sm:mx-auto">
         <div className="relative lg:w-1/2">
