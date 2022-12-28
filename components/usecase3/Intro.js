03319950587400198e7d4c89ec8e8491a22e3976
Satomi <s-tanaka@bcore.co.jp>
2022-12-28 16:00:37 +0900

    change p

--- components/usecase3/Intro.js
+++ components/usecase3/Intro.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Intro() {
     return (
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="px-4 py-20 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div className="py-16 grid gap-10 lg:grid-cols-2">
                 <div className="">
                     <Image
